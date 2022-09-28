4bdbc50dc943f14949430a56f31792dc92231186
jakecmeissner <59347388+jakecmeissner@users.noreply.github.com>
2022-09-28 16:45:35 -0400

    finalized picture removal

--- posts/admin.py
+++ posts/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from . import models
 from .models import Post
-from .models import Picture
 
 # Register your models here.
 admin.site.register(Post)
-admin.site.register(Picture)
