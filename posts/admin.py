ddb9053a36c80240ca377dd05c05fbcec9d67020
jakecmeissner <59347388+jakecmeissner@users.noreply.github.com>
2022-09-28 01:43:02 -0400

    new changes to project

--- posts/admin.py
+++ posts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from . import models
 from .models import Post
+from .models import Picture
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Picture)
--- posts/models.py
+++ posts/models.py
@@ -13,3 +13,16 @@ class Post(models.Model):
     createdAt = models.DateTimeField(
         'Created DateTime', blank=True,  auto_now_add=True
     )
+
+class Picture(models.Model):
+    class Meta(object):
+        db_table = 'picture'
+    name = models.CharField(
+        'Name', blank=False, null=False, max_length=14, db_index=True, default='Anonymous', 
+    )
+    body = models.CharField(
+        'Body', blank=True, null=True, max_length=140, db_index=True 
+    )
+    createdAt = models.DateTimeField(
+        'Created DateTime', blank=True,  auto_now_add=True
+    )
